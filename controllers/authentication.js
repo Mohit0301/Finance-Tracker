cb567d7f1678e5eaf44c04ae955a7c722b1b1a3c
Mohit Panchariya <iammohitpanchariya@gmail.com>
2023-03-27 23:43:24 +0530

    Setup register route
    Users can now register for the service.
    If the user is registered succesfully, json object
    with success set to true is sent back to the user.
    If there is a problem with the registration, an error is thrown
    which is handled by the error handling middleware.
    If the type of error is either BadRequestError, UnauthorizedError,
    or NotFound error, then the error handling middleware sends back
    a response with an appropriate status code.
    In case any other error is throw a generic error message, with success set
    to false is sent back to the user.

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,10 +1,40 @@
+const {BadRequestError} = require("../errors/");
+const bcrypt = require("bcrypt");
+const UserModel = require("../models/user-model");
+const {StatusCodes} = require("http-status-codes");
 
 async function login(req, res) {
     res.send("TODO");
 }
 
 async function register(req, res) {
-    res.send("TODO");
+    
+    const {firstName, lastName, email, password} = req.body;
+
+    if(!firstName || !lastName || !email || !password) {
+        throw new BadRequestError("First Name, Last Name, Email, and Password required.");
+    }
+
+    const newUser = await createUserObject(firstName, lastName, email, password);
+
+    const user = await UserModel.create(newUser);
+
+    res.status(StatusCodes.CREATED).json({success: true, user});
+}
+
+async function createUserObject(firstName, lastName, email, password) {
+    const newUser = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassowrd = await bcrypt.hash(password, salt);
+
+    newUser.password = hashedPassowrd;
+
+    return newUser;
 }
 
 module.exports = {login, register};
