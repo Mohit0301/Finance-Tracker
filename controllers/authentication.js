8fd98d84dd23a0efb9c94cf91378fb5608581823
Mohit Panchariya <iammohitpanchariya@gmail.com>
2023-03-28 00:34:18 +0530

    Setup login controller
    Users can now login to use the service.

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,10 +1,35 @@
-const {BadRequestError} = require("../errors/");
+const {BadRequestError, NotFoundError, UnauthorizedError} = require("../errors/");
 const bcrypt = require("bcrypt");
 const UserModel = require("../models/user-model");
 const {StatusCodes} = require("http-status-codes");
+const jwt = require("jsonwebtoken");
+
 
 async function login(req, res) {
-    res.send("TODO");
+    const {email, password} = req.body;
+
+    if(!email || !password) {
+        throw new BadRequestError("Email, and password are required");
+    }
+
+    const user = await UserModel.findOne({where: {
+        email: email
+    }});
+    
+    if(!user) {
+        throw new UnauthorizedError("Incorrect email or password");
+    }
+
+    const passwordIsCorrect = await bcrypt.compare(password, user.password);
+
+    if(passwordIsCorrect) {
+        const token = jwt.sign({userId: user.id, email: user.email}, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_LIFETIME
+        });
+        return res.status(StatusCodes.OK).json({success: true, token: token});
+    } else {
+        throw new UnauthorizedError("Incorrect email or password");
+    }
 }
 
 async function register(req, res) {
