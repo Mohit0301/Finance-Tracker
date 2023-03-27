1bb7d4c263abdb4cb11e1620c6335d4544a83d20
Mohit Panchariya <iammohitpanchariya@gmail.com>
2023-03-27 19:55:25 +0530

    Add registration date field to user model

--- models/user-model.js
+++ models/user-model.js
@@ -32,6 +32,10 @@ const User = sequelize.define("User", {
     password: {
         type: DataTypes.STRING,
         allowNull: false
+    },
+    registrationDate: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
     }
 });
 
