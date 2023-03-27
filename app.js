439701998793b225d7ab0fd8fa4c5c4c2e066ffe
Mohit Panchariya <iammohitpanchariya@gmail.com>
2023-03-27 22:27:13 +0530

    Setup authentication router
    Setup an authentication router which deals
    with the login and register routes.

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const app = express();
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const connectDB = require("./db/connect-db");
 const notFoundMiddleware = require("./middleware/not-found");
+const authenticationRouter = require("./routers/authentication");
 require("dotenv").config();
 require("express-async-errors");
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+app.use("/api/v1/authentication", authenticationRouter);
+
 app.use(errorHandlerMiddleware);
 app.use(notFoundMiddleware);
 
---
+++ routers/authentication.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/login", (req, res) => {
+    res.send("TODO");
+});
+
+router.post("/register", (req, res) => {
+    res.send("TODO");
+});
+
+module.exports = router;
