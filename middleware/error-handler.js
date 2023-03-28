a51a82195e3ec77d65912b61a8d33e5231d65547
Mohit Panchariya <iammohitpanchariya@gmail.com>
2023-03-28 10:35:12 +0530

    Improve error handler
    Error handler will now respond back with an appropriate
    error message when a sequelize validation error is thrown.

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,15 +1,19 @@
 const {BadRequestError, UnauthorizedError, NotFoundError} = require("../errors/");
 const {StatusCodes} = require("http-status-codes");
+const { ValidationError } = require("sequelize");
 
 function errorHandler(error, req, res, next) {
     const response = {
         success: false,
-        message: error.message
+        message: error.message || "Something went wrong"
     };
     if(error instanceof BadRequestError || error instanceof UnauthorizedError || error instanceof NotFoundError) {
         return res.status(error.statusCode).json(response);
+    } else if(error instanceof ValidationError) {
+        response.message = error.message;
+        return res.status(StatusCodes.BAD_REQUEST).json(response);
     }
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({success: false, message: "Something went wrong."});
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(response);
 }
 
 module.exports = errorHandler;
