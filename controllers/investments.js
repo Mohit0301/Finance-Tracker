ab2b68fd6d35a011840c05a404f50929d09aa05e
Mohit Panchariya <iammohitpanchariya@gmail.com>
2023-03-28 09:39:24 +0530

    Add controllers for investments
    Setup a basic skeleton for the required investment
    controllers.

---
+++ controllers/investments.js
@@ -0,0 +1,28 @@
+
+async function getAllInvestments(req, res) {
+    res.send("TODO: getAllInvestments");
+}
+
+async function getInvestment(req, res) {
+    res.send("TODO: getInvestment");
+}
+
+async function createInvestment(req, res) {
+    res.send("TODO: createInvestment");
+}
+
+async function updateInvestment(req, res) {
+    res.send("TODO: updateInvestment");
+}
+
+async function deleteInvestment(req, res) {
+    res.send("TODO: deleteInvestment");
+}
+
+module.exports = {
+    getAllInvestments, 
+    getInvestment,
+    createInvestment,
+    updateInvestment,
+    deleteInvestment
+};
